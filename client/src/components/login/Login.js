152be611a6a0db8c6d00d754a25fa8c664c51907
Kariuki <mkmartinoes@gmail.com>
2022-07-30 14:19:27 +0300

    Login skeleton

---
+++ client/src/components/login/Login.js
@@ -0,0 +1,20 @@
+import "./Login.css"
+function Login(){
+
+  return(
+    <div className="login">
+      <div className= "roww">
+        <div className="col-6 col-s-7 form">
+          <div className="header">
+            <p>Get Started</p>
+          </div>
+        </div>
+        <div className="col-6 col-s-5 display">
+    
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
