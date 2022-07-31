e19d4d0580c497edeba8c86026e0a4b0fd39e994
Kariuki <mkmartinoes@gmail.com>
2022-07-31 09:35:43 +0300

    st: signin ui design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Login from './components/login/Login';
+// import Login from './components/login/Login';
+import Signup from './components/signup/signup';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </div>
   );
 }
---
+++ client/src/components/signup/signup.js
@@ -0,0 +1,73 @@
+import "./signup.css";
+
+function Signup() {
+  return (
+    <div className="signup">
+      <div className="roww">
+        <div className="col-6 col-s-7 form">
+          <div className="header">
+            <p>Get Started</p>
+          </div>
+          <form>
+            <div className="username">
+              <span
+                className="iconify"
+                data-icon="icon-park-solid:edit-name"
+                style={{ color: "#f3a446" }}
+                data-width="48"
+                data-height="48"
+              ></span>
+              <input type="text" placeholder="Username" />
+            </div>
+            <hr/>
+            <div className="username">
+              <span
+                className="iconify"
+                data-icon="el:phone-alt"
+                style={{ color: "#f3a446" }}
+                data-width="48"
+                data-height="48"
+              ></span>
+              <input type="text" placeholder="Username" />
+            </div>
+            <hr />
+            <div className="username">
+              <span
+                className="iconify"
+                data-icon="bi:shield-lock"
+                style={{ color: "#f3a446" }}
+                data-width="48"
+                data-height="48"
+              ></span>
+              <input type="password" placeholder="password" />
+            </div>
+            <hr></hr>
+            <div className="buttons">
+              <p>Forgot Passord?</p>
+              <button type="submit">Sign In</button>
+            </div>
+          </form>
+        </div>
+        <div className="col-6 col-s-5 display">
+          <header>
+            <p>Welcome To</p>
+            <img src={require("./1.jpeg")} alt="swahili spot" />
+            <span>SWAHILI SPOT</span>
+          </header>
+          <div className="body">
+            <p>
+              Get the best of coastal meals in Kenya for value of your money
+            </p>
+          </div>
+          <div className="creator">
+            <span>Made With Love</span>
+            <span className="span"></span>
+            <span>Martin Kariuki</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
