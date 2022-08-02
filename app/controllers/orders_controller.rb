28bf930a0ee2bc5b401c5ab6013d5424a532a4d9
Kariuki <mkmartinoes@gmail.com>
2022-08-02 10:50:54 +0300

    ft: routes orders routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+  def index
+    product = Product.all
+    render json: product, include: :users, status: :ok
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product, status: :ok
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:name, :id, :image, :description, :price, :amount)
+  end
+
 end
