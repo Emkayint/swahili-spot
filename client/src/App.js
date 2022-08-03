435467c3cef75609e4b4b825f2baa0ee5ac01178
Kariuki <mkmartinoes@gmail.com>
2022-08-03 20:43:28 +0300

    Bugs: routing

--- client/src/App.js
+++ client/src/App.js
@@ -15,22 +15,19 @@ function App() {
   })
   return (
     <div className="App">
-      <Navbar user={user} setUser={setUser} />
-      <div className="routes">
-        <Routes>
-          <Route path="/dashboard" element={<Board user={user} />} />
-          <Route
-            path="/login"
-            element={<Login user={user} setUser={setUser} />}
-          />
-          <Route
-            path="/signin"
-            element={<Signup user={user} setUser={setUser} />}
-          />
-          <Route path="/cart" element={<Cart user={user} />} />
-          <Route path="/" element={<Menu user={user} />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/dashboard" element={<Board user={user} />} />
+        <Route
+          path="/login"
+          element={<Login user={user} setUser={setUser} />}
+        />
+        <Route
+          path="/signin"
+          element={<Signup user={user} setUser={setUser} />}
+        />
+        <Route path="/cart" element={<Cart user={user} />} />
+        <Route path="/" element={<Menu user={user} />} />
+      </Routes>
     </div>
   );
 }
