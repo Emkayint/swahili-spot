a02575d670536c70b7a93f9d1cda610d5bfb1ca8
Martin <mkmartinoes@gmail.com>
2022-09-05 07:54:46 +0300

    Jsom web token authenitication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,22 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_action :authorize
 
   private
 
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
-
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 
   def render_unprocessable_entity_response(exception)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,26 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: :create
 
+  # def create
+  #   user = User.find_by(username: params[:username])
+  #   # byebug
+  #   if user&.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     render json: user, status: :created
+  #   else
+  #     render json: {error: {login: "Invalid Username or password"}}, status: :unauthorized
+  #   end
+  # end
+
   def create
-    user = User.find_by(username: params[:username])
-    # byebug
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      render json: user, status: :created
+    @user = User.find_by_email(params[:email])
+    if @user && @user.password == params[:password]
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { jwt: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     name: @user.name, email: @user.email, role: @user.role }, status: :ok
     else
-      render json: {error: {login: "Invalid Username or password"}}, status: :unauthorized
+      render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
 
