d638f4ce6fd3ee71828f0fd84584a2c8d5b137fc
Martin <mkmartinoes@gmail.com>
2022-09-06 09:33:45 +0300

    bug: nil class erroe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
 
   skip_before_action :authorize_admin
 
-  @@phone_number = @current_user.phone
+  @@phone_number = if(@current_user) @current_user.phone end
   @@short_code = 174_379
   @@passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
   @@callback = "https://locahost/,,,"
